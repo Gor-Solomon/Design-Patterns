4042191a03a7843503d1e889d02973a3bbdd5aee
IDao <Talxolenia@users.noreply.github.com>
2018-02-02 17:11:44 +0400

    adding

---
+++ DecoratorPattern/ConsoleApplication3/Interfaces/ICost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3.Interfaces
+{
+    interface ICost
+    {
+        double GetCost();
+        string Description { get; set; }
+    }
+}
